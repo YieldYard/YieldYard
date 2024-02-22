7f2f8bffed1a4e52e54dac96bc39f5190bd24cbd
YieldYard <134914@proton.me>
2024-02-22 18:29:23 +0100

    Upgrades

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,10 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
